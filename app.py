e5feec256e8f0c73208e85e538eb9df4a9843d56
Uzaif-Sheikh <uzaifsheikh2309@gmail.com>
2021-06-22 20:07:31 +1000

    Added app.py

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
 from flask import Flask, app, request
 import requests
+from pymessenger.bot import Bot
 
 ACCESS_TOKEN = "EAANuV13vMV8BAEIvZAzOoJbNU4ytZCQYiyp6L8yOdlZCLtZAOBYcZCjzq8FMjJAvSX6FZBU8tOFbsq4FE5aZAN8ZB3VsoBCjmZC5AUD0PNoa16vkSGPqtFfZCqgn3Syj32hiOQbzlmxJoLRcJzItTSvH4ntRd6HMZCibFdMNvnFYLKTByxMEOaKXJUFPU5MmnHSFF2XJVUVZBptTVwZDZD"
 
 VERIFY_TOKEN = "colombo_house_token"
+
+bot = Bot(ACCESS_TOKEN)
 
 greetings = ["hi","hello","yo","oi","hey"]
 
 
 app = Flask(__name__)
 
-@app.route('/',methods=["GET"])
-def webhook_1():
+@app.route('/',methods=["GET","POST"])
+def check_message():
     if request.method == "GET":
+        print(request.args.get("hub.verify_token"))
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            print(request.args.get("hub.challenge"))
             return request.args.get("hub.challenge")
         else:
             return "Hello, Sorry"
 
     else:
+        data = request.get_json()
+        message = data['entry'][0]['messaging'][0]['message']
+        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+
+        try: 
+            message['text']
+        except KeyError:
+            response = webhook_2(sender_id)
+            return response
+
+        if message['text'].lower() in greetings:
+            response = webhook_1(sender_id)
+            return response
+        elif message['text'].lower() == "help":
+            response = webhook_3(sender_id)
+            return response
+        else:
+            response = webhook_2(sender_id)
+            return response
+
         return "200"
 
 
@@ -29,29 +54,29 @@ def webhook_1():
 #     return 'ok'
 
 
-@app.route("/", methods=['POST'])
-def check_message():
-    data = request.get_json()
-    message = data['entry'][0]['messaging'][0]['message']
-    sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+# @app.route("/", methods=['POST'])
+# def check_message():
+#     data = request.get_json()
+#     message = data['entry'][0]['messaging'][0]['message']
+#     sender_id = data['entry'][0]['messaging'][0]['sender']['id']
 
-    try: 
-        message['text']
-    except KeyError:
-        response = webhook_2(sender_id)
-        return response
+#     try: 
+#         message['text']
+#     except KeyError:
+#         response = webhook_2(sender_id)
+#         return response
 
-    if message['text'].lower() in greetings:
-        response = webhook_1(sender_id)
-        return response
-    elif message['text'].lower() == "help":
-        response = webhook_3(sender_id)
-        return response
-    else:
-        response = webhook_2(sender_id)
-        return response
+#     if message['text'].lower() in greetings:
+#         response = webhook_1(sender_id)
+#         return response
+#     elif message['text'].lower() == "help":
+#         response = webhook_3(sender_id)
+#         return response
+#     else:
+#         response = webhook_2(sender_id)
+#         return response
 
-    return 'ok'
+#     return 'ok'
 
 
 def webhook_1(sender_id):
@@ -87,4 +112,4 @@ def webhook_3(sender_id):
     return response
 
 if __name__ == "__main__":
-    app.run()
+    app.run()   
